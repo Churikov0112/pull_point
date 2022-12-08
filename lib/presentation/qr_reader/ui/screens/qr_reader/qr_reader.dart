402cf53effbf7b6f7d8406a8a54776882b9eed55
egor <churikov.egor@bk.ru>
2022-12-08 15:05:27 +0300

    qr scanner without navigation

--- lib/presentation/qr_reader/ui/screens/qr_reader/qr_reader.dart
+++ lib/presentation/qr_reader/ui/screens/qr_reader/qr_reader.dart
@@ -1,10 +1,48 @@
+import 'package:flutter/material.dart' show MaterialPageRoute;
+import 'package:mobile_scanner/mobile_scanner.dart';
+
 import 'package:flutter/widgets.dart';
 
-class QrReaderScreen extends StatelessWidget {
+import '../../../../artist/artist_guest_screen.dart';
+
+class QrReaderScreen extends StatefulWidget {
   const QrReaderScreen({Key? key}) : super(key: key);
 
   @override
+  State<QrReaderScreen> createState() => _QrReaderScreenState();
+}
+
+class _QrReaderScreenState extends State<QrReaderScreen> {
+  MobileScannerController cameraController = MobileScannerController();
+
+  @override
+  void dispose() {
+    cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    cameraController.start();
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text('QR');
+    print("cameraController.hashCode ${cameraController.hashCode}");
+    return MobileScanner(
+      controller: cameraController,
+      allowDuplicates: false,
+      onDetect: (barcode, args) {
+        if (barcode.rawValue == null) {
+          debugPrint('Failed to scan Barcode');
+        } else {
+          final String code = barcode.rawValue!;
+          debugPrint('Barcode found! $code');
+          // Navigator.of(context).push(MaterialPageRoute<void>(builder: (BuildContext context) => ArtistGuestScreen()));
+          cameraController.stop();
+        }
+      },
+    );
   }
 }
