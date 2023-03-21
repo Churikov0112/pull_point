f7f014671d43be61fa6849bf4dccb1d5b216ba65
Churikov Egor <churikov.egor@bk.ru>
2023-03-21 23:42:45 +0300

    remove id from wallet

--- lib/domain/models/wallet/wallet.dart
+++ lib/domain/models/wallet/wallet.dart
@@ -1,19 +1,16 @@
 part of '../models.dart';
 
 class WalletModel {
-  final int id;
   final int balance;
   final String cardNumber;
 
   const WalletModel({
-    required this.id,
     required this.balance,
     required this.cardNumber,
   });
 
   static WalletModel fromJson(dynamic source) {
     return WalletModel(
-      id: source['id'],
       balance: source['balance'],
       cardNumber: source['bankCredentials'],
     );
